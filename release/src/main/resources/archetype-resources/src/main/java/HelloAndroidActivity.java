6e363e9eed85513f5731c29e5c15f4f59a23deb0
Nikita Vorobej <mail@gamersroom.ru>
2013-01-11 17:56:35 +0300

    Initial commit.

---
+++ release/src/main/resources/archetype-resources/src/main/java/HelloAndroidActivity.java
@@ -0,0 +1,16 @@
+package ${package};
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class HelloAndroidActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+}
+
